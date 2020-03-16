c3dd68d8bc0b5765b1dcf310e728e449d74e1a50
balicaf <37401133+balicaf@users.noreply.github.com>
2020-03-16 15:47:54 +0100

    Update film.sh

--- film.sh
+++ film.sh
@@ -7,11 +7,11 @@ else
     exit
 fi
 
-python ~/begin.py & #power on bulbs
+python ./begin.py & #power on bulbs
 message=$(zenity --file-selection --filename=/media/balicaf/8To/film/) # user select his movie
 
 pkill -USR1 '^redshift$' #redshift is killed in order to have a 6500k screen
-python ~/end.py & #user has 60 seconds before bulbs are powered off
+python ./end.py & #user has 60 seconds before bulbs are powered off
 
 nvgstplayer -i "$message" -a 30 #movie is hardware decoded and starts at 30seconds
 #nvgstplayer -c 2                                   #choose 2nd audio traCk
