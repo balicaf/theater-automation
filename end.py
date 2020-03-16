e2d5865f855366174c9c6acead6ce61e863cf3ce
balicaf <37401133+balicaf@users.noreply.github.com>
2020-03-16 14:45:54 +0100

    Update end.py

--- end.py
+++ end.py
@@ -1,16 +1,19 @@
 import lazylights
 import time
 import threading
-#time.sleep(60)
+
 def callback():
+#To power off every bulbs connected to your router
 	bulbs = lazylights.find_bulbs()
+	lazylights.set_power(bulbs, False)
+	
+#To select only a 	
 #	bulbs_by_name = {state.label.strip(" \x00"): state.bulb
 #		for state in lazylights.get_state(bulbs)}
 #	lazylights.set_power([bulbs_by_name['Nice']], False)
 #	lazylights.set_power([bulbs_by_name['Right']], False)
-	lazylights.set_power(bulbs, False)
 
-timer = threading.Timer(60.0, callback)
+timer = threading.Timer(60.0, callback) #this is a non-blocking time.sleep(60)
 timer.start()  # after 60 seconds, 'callback' will be called
 
 
