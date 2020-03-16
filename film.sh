167aab2581535c7a6d0efc1a7cb47dea04982778
balicaf <37401133+balicaf@users.noreply.github.com>
2020-03-16 14:51:54 +0100

    Update film.sh

--- film.sh
+++ film.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+# at startup, on screen a GUI appears
 if zenity --question --text="Are you sure to watch movie?"
 then
     continue
 else
     exit
 fi
-python ~/begin.py &
-message=$(zenity --file-selection --filename=/media/balicaf/8To/film/)
-echo "nvgstplayer -i $message"
-pkill -USR1 '^redshift$'
-python ~/end.py &
-# & not to wait the end of that python
-nvgstplayer -i "$message" -a 30
-# begin at 30 seconds  -c 2 choose 2nd audio traCk
-# mediainfo $message --inform="Audio;%Language%"
+
+python ~/begin.py & #power on bulbs
+message=$(zenity --file-selection --filename=/media/balicaf/8To/film/) # user select his movie
+
+pkill -USR1 '^redshift$' #redshift is killed in order to have a 6500k screen
+python ~/end.py & #user has 60 seconds before bulbs are powered off
+
+nvgstplayer -i "$message" -a 30 #movie is hardware decoded and starts at 30seconds
+#nvgstplayer -c 2                                   #choose 2nd audio traCk
+# mediainfo $message --inform="Audio;%Language%"    #displays available languages
